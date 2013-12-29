4597ee56e43b3d846da1f9a160144e99ed3d19f3
JosephAntony <kjantony1988@gmail.com>
2013-12-29 23:33:29 +0530

    second commit

--- WebContent/js/clearTripRouter.js
+++ WebContent/js/clearTripRouter.js
@@ -12,13 +12,32 @@ define(['jquery', 'underscore', 'backbone', 'bootstrap', 'mustache', 'abstractMo
 			this.filter =  null;
 		},
 		home: function(){
+			$("#filter").attr("style", "display:none");
 			var fareCalculatorModel = new fareModel.getFareModel();
 			$("div.cart").append( new fareView.getFareView({model: fareCalculatorModel}).el );
 			$("div.cart").append('<div class="buy"><button>Book</button></div>');
-			this.trip1List = new tripListView.getTripListView({url: '../json/MNflights.json', viewId: 'trip1-list', model: fareCalculatorModel});
+			if(this.trip1List != null){
+				this.trip1List.remove();
+				this.trip1List = new tripListView.getTripListView({url: '../json/MNflights.json', viewId: 'trip1-list', model: fareCalculatorModel});
+				$(".trip").append(this.trip1List.el);
+			}else {
+				this.trip1List = new tripListView.getTripListView({url: '../json/MNflights.json', viewId: 'trip1-list', model: fareCalculatorModel});
+				$(".trip").append(this.trip1List.el);
+			}
+			
+			if(this.trip2List != null){
+				this.trip2List.remove();
+				this.trip2List = new tripListView.getTripListView({url: '../json/NMflights.json', viewId: 'trip2-list', model: fareCalculatorModel});
+				$(".trip").append(this.trip2List.el);
+			}else{
+				this.trip2List = new tripListView.getTripListView({url: '../json/NMflights.json', viewId: 'trip2-list', model: fareCalculatorModel});
+				$(".trip").append(this.trip2List.el);
+			}
+			
+			/*this.trip1List = new tripListView.getTripListView({url: '../json/MNflights.json', viewId: 'trip1-list', model: fareCalculatorModel});
 			$(".trip").append(this.trip1List.el);
 			this.trip2List = new tripListView.getTripListView({url: '../json/NMflights.json', viewId: 'trip2-list', model: fareCalculatorModel});
-			$(".trip").append(this.trip2List.el);
+			$(".trip").append(this.trip2List.el);*/
 			
 		},
 		
